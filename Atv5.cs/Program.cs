902bbf8de21d32b212d287fa9052760a4d6df47b
Stephanie Campos <118989596+Stephanie-Camp@users.noreply.github.com>
2023-12-26 12:59:05 -0300

    Update Program.cs

--- Atv5.cs/Program.cs
+++ Atv5.cs/Program.cs
@@ -2,7 +2,7 @@
     class Program{
         static void Main(string[] args){
             string myName = "\u0053\u0074\u0065\u0070\u0068\u0061\u006E\u0069\u0065\u0020\u0043\u0061\u006D\u0070\u006F\u0073\u0020\u0064\u0065\u0020\u0041\u006C\u006D\u0065\u0069\u0064\u0061";
-            Console.WriteLine("Meu chamo " + myName);
+            Console.WriteLine("Me chamo " + myName);
         }
     }
 }
