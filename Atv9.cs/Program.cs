5d549e81bbfddbb23bd3c1771ef1cb184e21e38b
Stephanie <stca.almeida@gmail.com>
2024-03-03 00:24:54 -0300

    override /work one

--- Atv9.cs/Program.cs
+++ Atv9.cs/Program.cs
@@ -2,8 +2,6 @@
     class Program {
         static void Main(string[] args) {
 
-            Ponto novoPonto = new Ponto();
-
             Ponto novoPonto2 = new Ponto();
             novoPonto2.CoordenadaX(5);
             Ponto novoPonto3 = new Ponto();
